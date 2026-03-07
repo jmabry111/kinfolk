85f85aff29dc857e870688038bd46325ec9f3a6d
Jason Mabry <jmabry@ymabry.com>
2026-03-07 05:30:52 -0500

    fix double class declaratoin

--- app/Http/Middleware/EnsureUserIsActive.php
+++ app/Http/Middleware/EnsureUserIsActive.php
@@ -19,22 +19,3 @@ class EnsureUserIsActive
         return $next($request);
     }
 }
-
-namespace App\Http\Middleware;
-
-use Closure;
-use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
-
-class EnsureUserIsActive
-{
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
-    public function handle(Request $request, Closure $next): Response
-    {
-        return $next($request);
-    }
-}
