46fc1d4a6e1634d09193417d5ccca81d53b3ebcc
Jason Mabry <jmabry@mymabry.com>
2026-03-11 10:42:33 -0400

    Change birthday reminders schedule to 11:00 AM

--- routes/console.php
+++ routes/console.php
@@ -9,4 +9,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('kinfolk:send-birthday-reminders')->dailyAt('10:30');
+Schedule::command('kinfolk:send-birthday-reminders')->dailyAt('11:00');
